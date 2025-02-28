01fcb42f8dbd0c7bc8c60114f94545585ec485fc
Khoirul Anam <anams908004@gmail.com>
2025-02-28 16:03:45 +0700

    pertemuan 2

---
+++ pertemuan2/variable_konstan.php
@@ -0,0 +1,19 @@
+<?php
+    // mendefinisikan konstanta
+    define("PHI", 3.14);
+    define("DBNAME", "Inventory");
+    define("OBSERVER", "Localhost");
+
+    $jari = 8;
+    $luas = PHI * $jari * $jari;
+    $kell = 2 * PHI * $jari;
+
+    echo "Luas Lingkaran dengan Jari " ,$jari, " : ", $luas;
+    echo "<br/> Kelilingnya : ", $kell;
+
+?>
+<hr>
+<?php
+    echo " Nama Database : " . DBNAME;
+    echo "<br/> Lokasi Database :" . OBSERVER;
+?>
